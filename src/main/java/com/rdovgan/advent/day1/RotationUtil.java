22ef61f4877e370d93fd5fb134acb517b053caed
Roman Dovgan <romadovg@gmail.com>
2025-12-06 20:25:02 +0200

    Improved Day 1

--- src/main/java/com/rdovgan/advent/day1/RotationUtil.java
+++ src/main/java/com/rdovgan/advent/day1/RotationUtil.java
@@ -19,10 +19,18 @@ public class RotationUtil {
 
 	public static void rotate(Password password, String rotation) {
 		int rotationValue = defineRotation(rotation);
-		password.setPosition((password.getPosition() + rotationValue) % 100);
-		if (password.getPosition() == 0) {
-			password.setPassword(password.getPassword() + 1);
+		if (rotationValue == 0) {
+			return;
 		}
+		int oldValue = password.getPosition();
+		int absValue = Math.abs(rotationValue);
+		int clicks = absValue / 100;
+		int rem = absValue % 100;
+		if (rem != 0 && (rotationValue > 0 ? oldValue + rem >= 100 : oldValue - rem < 0)) {
+			clicks++;
+		}
+		password.setPosition(Math.floorMod(oldValue + rotationValue, 100));
+		password.setPassword(password.getPassword() + clicks);
 	}
 
 }
